31aecec7f881f30706e1a739f4d57859b449aca6
Sheheer Ali <67970285+SHEHEERALIZX@users.noreply.github.com>
2022-10-29 17:43:44 +0530

    added readInput_example.sh
    #hacktoberfest

---
+++ examples/readInput_example.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#Read Input Example
+
+# and print the output
+
+read -p "What is your name:" name
+read -p "Enter Roll Number:" rollno
+echo "Entered name is : "$name
+echo "Roll number is : "$rollno
+
